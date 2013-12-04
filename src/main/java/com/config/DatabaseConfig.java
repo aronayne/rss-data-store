14ea1d3ffe127c2554131e9ec1a1a427ee6accf6
adrian-git-hub-user <adrian.ronayne@gmail.com>
2013-12-04 22:56:27 +0000

    update

--- src/main/java/com/config/DatabaseConfig.java
+++ src/main/java/com/config/DatabaseConfig.java
@@ -8,15 +8,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseConfig {
 
-    @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-
-        BasicDataSource basicDataSource = new BasicDataSource();
+	private static BasicDataSource basicDataSource;
+	
+	static {
+		basicDataSource = new BasicDataSource();
         basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
         basicDataSource.setUrl("jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_fca06dcb390cb0f?reconnect=true");
         basicDataSource.setUsername("b48516fc807a21");
         basicDataSource.setPassword("66a715a2");
-
+	}
+	
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
         return basicDataSource;
     }
 }
